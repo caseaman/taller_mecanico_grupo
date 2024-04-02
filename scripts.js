8c1ef6aeb823a47eda02ba7cb7e5f7342e48f5f4
Constanza Mena <co.mena@duocuc.cl>
2024-04-02 20:56:27 -0300

    1.1.01
    proyecto rayo maqueen

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('openCalendar').addEventListener('click', function() {
+    var calendarContainer = document.getElementById('calendarContainer');
+    calendarContainer.classList.toggle('hidden');
+});
