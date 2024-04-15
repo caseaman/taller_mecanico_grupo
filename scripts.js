b4b205695570d07b782bf10fbf9190eed0208aa9
caseaman <165098332+caseaman@users.noreply.github.com>
2024-04-14 22:17:59 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
+// JavaScript para la funcionalidad de la caja de búsqueda
+document.addEventListener('DOMContentLoaded', function() {
+  const searchInput = document.querySelector('.search-box input[type="text"]');
+  const searchButton = document.querySelector('.search-box button');
+
+  searchButton.addEventListener('click', function() {
+    const searchTerm = searchInput.value.trim();
+    if (searchTerm !== '') {
+      // Aquí podrías implementar la lógica para realizar la búsqueda
+      alert('Buscando: ' + searchTerm);
+    } else {
+      alert('Por favor, ingresa un término de búsqueda válido.');
+    }
+  });
+});
+
+// JavaScript para la funcionalidad del calendario
 document.getElementById('openCalendar').addEventListener('click', function() {
     var calendarContainer = document.getElementById('calendarContainer');
     calendarContainer.classList.toggle('hidden');
